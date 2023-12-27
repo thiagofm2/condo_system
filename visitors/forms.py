ba00cf604d2b6b289fd541813c3b772ad95f9fb8
thiagofm2 <thiago.fm012@gmail.com>
2023-12-27 10:26:29 -0300

    returning success and error messages

--- visitors/forms.py
+++ visitors/forms.py
@@ -4,4 +4,25 @@ from visitors.models import Visitor
 class VisitorsForm(forms.ModelForm):        ## Criamos uma classe que será subclasse do forms.ModelForm padrão do Django.
     class Meta:
         model = Visitor                     ## Indicamos o modelo que deve se basear para gerar os campos do formulário
-        fields = "__all__"                  ## Usaremos todos os campos de nosso modelo
+        fields = [                          ## Iremos indicar os campos que queremos que apareça através de uma lista contendo os nomes dos atributos
+            "complete_name",
+            "document",
+            "birthdate",
+            "home_number",
+            "license_plate"
+        ]
+        error_messages = {
+            "complete_name": {
+                "required": "That field is required."
+            },
+            "document": {
+                "required": "That field is required."
+            },
+            "birthdate": {
+                "required": "That field is required.",
+                "invalid": "The format of birthdate is wrong. The format expected is dd/mm/yyyy"
+            },
+            "home_number": {
+                "required": "That field is required."
+            }
+        }
--- visitors/views.py
+++ visitors/views.py
@@ -1,8 +1,26 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
 from visitors.forms import VisitorsForm
 
 def register_visitor(request):
     form = VisitorsForm()
+    
+    if request.method == 'POST':                    ## Verificamos de o método da requisição é POST
+        form = VisitorsForm(request.POST)           ## Caso seja, utilizaremos os campos do nosso modelo como body da requisição
+        if form.is_valid():                         ## Conferimos se os campos estão preenchidos e válidos
+            
+            visitor = form.save(commit=False)       ## Criamos uma variável que recebe o método save, usando o argumento de commit=False, que salva o corpo da requisição, mas que não envia pro servidor ainda.
+            
+            visitor.concierge_responsible = request.user.concierge              # Passamos as informações do porteiro logado na sessão para serem utilizadas como concierge responsável pelo cadastro.
+            visitor.save()                                                      ## Só depois disso salvamos de fato para enviar para o servidor.
+            
+            messages.success(                                   ## Mensagem de sucesso caso a requisição dê certo.
+                request,
+                "Visitor has been succesful registred"
+            )
+            
+            return redirect("index")
+            
     context =  {
         "page_name":"Register Visitor",
         "form": form
